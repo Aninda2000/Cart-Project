89eb57a660bac92b1eb5c9f9024a3b2875f6b51e
Aninda2000 <anindaroy100@gmail.com>
2022-08-20 14:28:46 +0530

    completing the cart project

--- src/App.js
+++ src/App.js
@@ -1,46 +1,80 @@
 import React from 'react';
 import Cart from './Cart';
 import Navbar from './Navbar';
+import * as firebase from 'firebase';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      products: [
-        {
-          price: 99,
-          title: 'Watch',
-          qty: 1,
-          img: 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MKUQ3_VW_34FR+watch-45-stainless-graphite-cell-7s_VW_34FR_WF_CO?wid=750&hei=712&trim=1,0&fmt=p-jpg&qlt=95&.v=1632171067000,1645753506827',
-          id: 1
-        },
-        {
-          price: 999,
-          title: 'Mobile Phone',
-          qty: 10,
-          img: 'https://images-na.ssl-images-amazon.com/images/I/71zGrrAe5NL._AC._SR360,460.jpg',
-          id: 2
-        },
-        {
-          price: 999,
-          title: 'Laptop',
-          qty: 4,
-          img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYrGFNtsAUTYNnN-iKUDcc5Z0ZD88uc-U-2w&usqp=CAU',
-          id: 3
-        }
-      ]
+      products: [],
+      loading:true
     }
-    // this.increaseQuantity = this.increaseQuantity.bind(this);
-    // this.testing();
+    this.db =firebase.firestore();
+  }
+  componentDidMount (){
+    // firebase
+    //   .firestore()
+    //   .collection('products')//this will return collection
+    //   .get()//this will return a promise
+    //   .then((snapshot)=>{
+    //     console.log(snapshot);
+    //     snapshot.docs.map((doc)=>{
+    //       console.log(doc.data());
+    //     });
+    //     const products = snapshot.docs.map((doc)=>{
+
+    //       const data =doc.data();
+    //       data['id']= doc.id;
+    //       return data;
+    //     });
+
+    //     this.setState({
+    //       products :products,
+    //       loading :false
+    //     })
+    //   })
+    this.db
+      .collection('products')//this will return collection
+      // .where('price','>=',999)
+      .orderBy('price','desc')
+      .onSnapshot((snapshot)=>{
+        console.log(snapshot);
+        snapshot.docs.map((doc)=>{
+          console.log(doc.data());
+        });
+        const products = snapshot.docs.map((doc)=>{
+
+          const data =doc.data();
+          data['id']= doc.id;
+          return data;
+        });
+
+        this.setState({
+          products :products,
+          loading :false
+        })
+      })
   }
   handleIncreaseQuantity = (product) => {
     console.log('increase the quantity', product);
     const { products } = this.state;
     const index = products.indexOf(product);
 
-    products[index].qty += 1;
-    this.setState({
-      products: products
+    // products[index].qty += 1;
+    // this.setState({
+    //   products: products
+    // })
+    const docRef =this.db.collection('products').doc(products[index].id);
+    docRef
+    .update({
+      qty:products[index].qty+1
+    })
+    .then(()=>{
+      console.log('Updated successfully');
+    })
+    .catch((err)=>{
+      console.log('Error ' ,err);
     })
   }
 
@@ -50,20 +84,43 @@ class App extends React.Component {
     if (products[index].qty == 0) {
       return;
     }
-    products[index].qty -= 1;
-    this.setState({
-      products
+    // products[index].qty -= 1;
+    // this.setState({
+    //   products
+    // })
+    const docRef =this.db.collection('products').doc(products[index].id);//find by id of the prodct in products array
+    docRef
+    .update({
+      qty :products[index].qty-1
     })
+    .then(()=>{
+      console.log('updated Successfully');
+    })
+    .catch((err)=>{
+      console.log('Error :', err);
+    })
+
   }
 
   handleDeleteProduct = (id) => {
     const { products } = this.state;
 
-    const items = products.filter((item) => item.id !== id);
-    this.setState({
-      products: items
+    // const items = products.filter((item) => item.id !== id);
+    // this.setState({
+    //   products: items
+    // })
+    const docRef =this.db.collection('products').doc(id);
+
+    docRef
+    .delete()
+    .then(()=>{
+      console.log('Deleted Successfully');
     })
+    .catch((err)=>{
+      console.log("Error" , err);
+    });
   }
+
   getCartCount=()=>{
     const {products} =this.state;
 
@@ -83,17 +140,35 @@ class App extends React.Component {
     });
     return total;
   }
+  addProduct=()=>{
+    this.db
+      .collection('products')
+      .add({
+        img:'',
+        price:9900,
+        qty:1,
+        title: 'Washing Machine'
+      })
+      .then((docRef)=>{
+        console.log('product has been added', docRef);
+      })
+      .catch((error)=>{
+        console.log('Error' ,error);
+      })
+  }
   render() {
-    const {products}= this.state;
+    const {products,loading}= this.state;
     return (
       <div className="App">
         <Navbar count={this.getCartCount()} />
+        <button onClick={this.addProduct} style={{padding:20, fontSize:20}}> Add a Product</button>
         <Cart
           products={products}
           onIncreaseQuantity={this.handleIncreaseQuantity}
           onDecreaseQuantity={this.handleDecreaseQuantity}
           onDeleteProduct={this.handleDeleteProduct}
         />
+        {loading && <h1> Loading Products... </h1>}
         <div style={{padding:20,fontSize:'28px'}}>Total: {this.getCartTotal()}</div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,5 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { initializeApp } from "firebase/app";
+// import * as firebase from 'firebase';
+// import 'firebase/firestore';
+
+// firebase setup
+const firebaseConfig = {
+    apiKey: "AIzaSyB3oGtZEFJ4V1-t_0zyqehhnmrJ3xdlR6g",
+    authDomain: "cart-ba82c.firebaseapp.com",
+    projectId: "cart-ba82c",
+    storageBucket: "cart-ba82c.appspot.com",
+    messagingSenderId: "112430680099",
+    appId: "1:112430680099:web:850cb632491ce815130996"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
